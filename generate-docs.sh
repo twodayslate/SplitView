ad7db8eb1960c531d6ed1c16827b4d59035d9c79
twodayslate <zac@gorak.us>
2019-09-04 16:22:48 -0400

    updated docs

---
+++ generate-docs.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+
+command -v jazzy
+
+if [ $? != 0 ]; then
+    echo "jazzy not found. Install jazzy:"
+    echo "\t[sudo] gem install jazzy"
+    exit 1
+fi
+
+set -e # don't print
+
+module="SplitView"
+github="twodayslate/SplitView"
+project="splitview.xcodeproj"
+scheme="SplitView"
+
+# get version number from podspec
+version="$(egrep "^\s*s.version\s*" SplitView.podspec | awk '{ gsub("\"", "", $3);  print $3 }')"
+today="$(date '+%Y-%m-%d')"
+
+if git rev-parse "v$version" >/dev/null 2>&1; then
+    # Use the tagged commit when we have one
+    ref="v$version"
+else
+    # Otherwise, use the current commit.
+    ref="$(git rev-parse HEAD)"
+fi
+
+# since tagging releases doesn't happen very much - let's just use head
+ref="master"
+
+jazzy \
+    --clean \
+    --github_url "https://github.com/$github" \
+    --github-file-prefix "https://github.com/$github/tree/$ref" \
+    --module-version "$version" \
+    --xcodebuild-arguments "-project,$project,-scheme,$scheme" \
+    --module "$module" \
+    --root-url "https://twodayslate.github.io/$module/reference/" \
+    --theme fullwidth \
+    --output docs \
+    --min-acl public\
+    --hide-documentation-coverage\
+    --copyright "[© 2019 SplitView](https://github.com/twodayslate/SplitView/blob/master/LICENSE). (Last updated: $today)" \
+    --author "twodayslate" \
+    --author_url "https://github.com/twodayslate" \
